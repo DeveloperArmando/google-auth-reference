81c3b05ad4def905d3fa15970595eec840d09183
Armando Deveeloper <armando.deveeloper@gmail.com>
2026-06-15 21:29:55 -0600

    feat: inject Google clientId via .env using @ngx-env/builder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleClientId: 'TU_CLIENT_ID.apps.googleusercontent.com',
+  googleClientId: import.meta.env['NG_APP_GOOGLE_CLIENT_ID'] as string,
 };
